6c1c3a9dc4de5b567c204ee42e98288c9a068fe6
chairil ashar <asharchairil4@gmail.com>
2023-04-25 22:31:42 +0700

    initial code

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,44 @@
+import {StyleSheet, Text, View, StatusBar, Image} from 'react-native';
+import React, {useEffect} from 'react';
+import {Colors, Images, Fonts} from '../constants';
+import {Display} from '../utils';
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Welcome');
+    }, 3000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={Colors.DEFAULT_GREEN}
+        translucent
+      />
+      <Image source={Images.PLATE} resizeMode="contain" style={styles.image} />
+      <Text style={styles.titleText}>FoodDelivery</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.DEFAULT_GREEN,
+  },
+  image: {
+    height: Display.setHeight(30),
+    width: Display.setWidth(60),
+  },
+  titleText: {
+    color: Colors.DEFAULT_WHITE,
+    fontSize: 32,
+    fontFamily: Fonts.POPPINS_LIGHT,
+  },
+});
