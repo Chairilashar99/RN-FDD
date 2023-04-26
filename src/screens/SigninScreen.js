9203accd76723ae8a3baeb5a9371ccc3759fba5d
chairil ashar <asharchairil4@gmail.com>
2023-04-26 23:41:18 +0700

    signin screen

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -6,106 +6,114 @@ import {
   TextInput,
   TouchableOpacity,
   Image,
+  ScrollView,
 } from 'react-native';
 import React from 'react';
 import {Separator} from '../components';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Feather from 'react-native-vector-icons/Feather';
-import {Colors, Fonts} from '../constants';
+import {Colors, Fonts, Images} from '../constants';
 import {Display} from '../utils';
 
 const SigninScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor={Colors.DEFAULT_WHITE}
-        translucent
-      />
-      <Separator height={StatusBar.currentHeight} />
-      <View style={styles.headerContainer}>
-        <Ionicons
-          name="chevron-back-outline"
-          size={30}
-          onPress={() => navigation.goBack()}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor={Colors.DEFAULT_WHITE}
+          translucent
         />
-        <Text style={styles.headerTitle}>Sign In</Text>
-      </View>
-      <Text style={styles.title}>Welcome</Text>
-      <Text style={styles.content}>
-        Enter your username and password, and enjoy ordering food
-      </Text>
-      <View style={styles.inputContainer}>
-        <View style={styles.inputSubContainer}>
-          <Feather
-            name="user"
-            size={22}
-            color={Colors.DEFAULT_GREY}
-            style={{marginRight: 10}}
+        <Separator height={StatusBar.currentHeight} />
+        <View style={styles.headerContainer}>
+          <Ionicons
+            name="chevron-back-outline"
+            size={30}
+            color={Colors.DEFAULT_BLACK}
+            onPress={() => navigation.goBack()}
           />
-          <TextInput
-            placeholder="Username"
-            placeholderTextColor={Colors.DEFAULT_GREY}
-            selectionColor={Colors.DEFAULT_GREY}
-            style={styles.inputText}
-          />
+          <Text style={styles.headerTitle}>Sign In</Text>
         </View>
-      </View>
-      <Separator height={15} />
-      <View style={styles.inputContainer}>
-        <View style={styles.inputSubContainer}>
-          <Feather
-            name="lock"
-            size={22}
-            color={Colors.DEFAULT_GREY}
-            style={{marginRight: 10}}
-          />
-          <TextInput
-            secureTextEntry={true}
-            placeholder="Password"
-            placeholderTextColor={Colors.DEFAULT_GREY}
-            selectionColor={Colors.DEFAULT_GREY}
-            style={styles.inputText}
-          />
-          <Feather
-            name="eye"
-            size={22}
-            color={Colors.DEFAULT_GREY}
-            style={{marginRight: 10}}
-          />
-        </View>
-      </View>
-      <Text></Text>
-      <View style={styles.forgotPasswordContainer}>
-        <View>
-          <Text>Remember me</Text>
-        </View>
-        <Text>Forgot Password</Text>
-      </View>
-      <TouchableOpacity>
-        <Text>Sign In</Text>
-      </TouchableOpacity>
-      <View>
-        <Text>Don't have an account</Text>
-        <Text>Sign Up</Text>
-      </View>
-      <Text>OR</Text>
-      <TouchableOpacity>
-        <View>
-          <View>
-            <Image />
+        <Text style={styles.title}>Welcome</Text>
+        <Text style={styles.content}>
+          Enter your username and password, and enjoy ordering food
+        </Text>
+        <View style={styles.inputContainer}>
+          <View style={styles.inputSubContainer}>
+            <Feather
+              name="user"
+              size={22}
+              color={Colors.DEFAULT_GREY}
+              style={{marginRight: 10}}
+            />
+            <TextInput
+              placeholder="Username"
+              placeholderTextColor={Colors.DEFAULT_GREY}
+              selectionColor={Colors.DEFAULT_GREY}
+              style={styles.inputText}
+            />
           </View>
-          <Text>Connect With Facebook</Text>
         </View>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <View>
-          <View>
-            <Image />
+        <Separator height={15} />
+        <View style={styles.inputContainer}>
+          <View style={styles.inputSubContainer}>
+            <Feather
+              name="lock"
+              size={22}
+              color={Colors.DEFAULT_GREY}
+              style={{marginRight: 10}}
+            />
+            <TextInput
+              secureTextEntry={true}
+              placeholder="Password"
+              placeholderTextColor={Colors.DEFAULT_GREY}
+              selectionColor={Colors.DEFAULT_GREY}
+              style={styles.inputText}
+            />
+            <Feather
+              name="eye"
+              size={22}
+              color={Colors.DEFAULT_GREY}
+              style={{marginRight: 10}}
+            />
           </View>
-          <Text>Connect With Google</Text>
         </View>
-      </TouchableOpacity>
+        <Text></Text>
+        <View style={styles.forgotPasswordContainer}>
+          <View>
+            <Text style={styles.rememberMeText}>Remember me</Text>
+          </View>
+          <Text style={styles.forgotPasswordText}>Forgot Password</Text>
+        </View>
+        <TouchableOpacity style={styles.signinButton}>
+          <Text style={styles.signinButtonText}>Sign In</Text>
+        </TouchableOpacity>
+        <View style={styles.signupContainer}>
+          <Text style={styles.accountText}>Don't have an account</Text>
+          <Text style={styles.signupText}>Sign Up</Text>
+        </View>
+        <Text style={styles.orText}>OR</Text>
+        <TouchableOpacity style={styles.facebookButton}>
+          <View style={styles.socialButtonContainer}>
+            <View style={styles.signinButtonLogoContainer}>
+              <Image source={Images.FACEBOOK} style={styles.signinButtonLogo} />
+            </View>
+            <Text style={styles.socialSigninButtonText}>
+              Connect With Facebook
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.googleButton}>
+          <View style={styles.socialButtonContainer}>
+            <View style={styles.signinButtonLogoContainer}>
+              <Image source={Images.GOOGLE} style={styles.signinButtonLogo} />
+            </View>
+            <Text style={styles.socialSigninButtonText}>
+              Connect With Google
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
@@ -115,7 +123,7 @@ export default SigninScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.DEFAULT_BLACK,
+    backgroundColor: Colors.DEFAULT_WHITE,
   },
   headerContainer: {
     flexDirection: 'row',
@@ -129,24 +137,24 @@ const styles = StyleSheet.create({
     lineHeight: 20 * 1.4,
     width: Display.setWidth(80),
     textAlign: 'center',
-    // color: Colors.DEFAULT_BLACK,
+    color: Colors.DEFAULT_BLACK,
   },
   title: {
     fontSize: 20,
     fontFamily: Fonts.POPPINS_MEDIUM,
     lineHeight: 20 * 1.4,
-    marginTop: 50,
+    marginTop: 25,
     marginBottom: 10,
-    marginHorizontal: 20,
-    // color: Colors.DEFAULT_BLACK,
+    marginHorizontal: 27,
+    color: Colors.DEFAULT_BLACK,
   },
   content: {
     fontSize: 20,
     fontFamily: Fonts.POPPINS_MEDIUM,
     marginTop: 10,
     marginBottom: 20,
-    marginHorizontal: 20,
-    // color: Colors.DEFAULT_BLACK,
+    marginHorizontal: 27,
+    color: Colors.DEFAULT_BLACK,
   },
   inputContainer: {
     backgroundColor: Colors.LIGHT_GREY2,
@@ -174,5 +182,102 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+  },
+  rememberMeText: {
+    marginLeft: 10,
+    fontSize: 12,
+    lineHeight: 12 * 1.4,
+    color: Colors.DEFAULT_GREY,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+  },
+  forgotPasswordText: {
+    fontSize: 12,
+    lineHeight: 12 * 1.4,
+    color: Colors.DEFAULT_GREEN,
+    fontFamily: Fonts.POPPINS_BOLD,
+  },
+  signinButton: {
+    backgroundColor: Colors.DEFAULT_GREEN,
+    borderRadius: 8,
+    marginHorizontal: 20,
+    height: Display.setHeight(6),
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  signinButtonText: {
+    fontSize: 18,
+    lineHeight: 18 * 1.4,
+    color: Colors.DEFAULT_WHITE,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+  },
+  signupContainer: {
+    marginHorizontal: 20,
+    justifyContent: 'center',
+    paddingVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  accountText: {
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+    color: Colors.DEFAULT_WHITE,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+    color: Colors.DEFAULT_BLACK,
+  },
+  signupText: {
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+    color: Colors.DEFAULT_GREEN,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+    marginLeft: 5,
+  },
+  orText: {
+    fontSize: 15,
+    lineHeight: 15 * 1.4,
+    color: Colors.DEFAULT_BLACK,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+    marginLeft: 5,
+    alignSelf: 'center',
+  },
+  facebookButton: {
+    backgroundColor: Colors.FABEBOOK_BLUE,
+    paddingVertical: 15,
+    marginHorizontal: 20,
+    borderRadius: 8,
+    marginVertical: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  googleButton: {
+    backgroundColor: Colors.GOOGLE_BLUE,
+    paddingVertical: 15,
+    marginHorizontal: 20,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signinButtonLogo: {
+    height: 18,
+    width: 18,
+  },
+  signinButtonLogoContainer: {
+    backgroundColor: Colors.DEFAULT_WHITE,
+    padding: 2,
+    borderRadius: 3,
+    position: 'absolute',
+    left: 25,
+  },
+  socialButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  socialSigninButtonText: {
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+    color: Colors.DEFAULT_WHITE,
+    fontFamily: Fonts.POPPINS_MEDIUM,
   },
 });
