68816606f8c5c163fe7637e1a188230432362b3b
chairil ashar <asharchairil4@gmail.com>
2023-04-29 16:40:23 +0700

    dropdown flag

--- src/screens/RegisterPhoneScreen.js
+++ src/screens/RegisterPhoneScreen.js
@@ -6,16 +6,24 @@ import {
   Image,
   TouchableOpacity,
   TextInput,
+  FlatList,
 } from 'react-native';
-import React from 'react';
-import {Colors, Fonts, Images} from '../constants';
-import {Separator} from '../components';
+import React, {useState} from 'react';
+import {Colors, Fonts, Images, CountryCode} from '../constants';
+import {FlagItem, Separator} from '../components';
 import {Display} from '../utils';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {StaticImageService} from '../services';
 
+const getDropdownStyle = y => ({...styles.countryDropdown, top: y + 60});
+
 const RegisterPhoneScreen = ({navigation}) => {
+  const [selectedCountry, setSelectedCountry] = useState(
+    CountryCode.find(country => country.name === 'Indonesia'),
+  );
+  const [inputContainerY, setInputContainerY] = useState(0);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   return (
     <View style={styles.container}>
       <StatusBar
@@ -37,19 +45,52 @@ const RegisterPhoneScreen = ({navigation}) => {
       <Text style={styles.content}>
         Enter your registered phone number to login.
       </Text>
-      <View style={styles.inputContainer}>
-        <TouchableOpacity style={styles.countryListContainer}>
+      <View
+        style={styles.inputContainer}
+        onLayout={({
+          nativeEvent: {
+            layout: {y},
+          },
+        }) => setInputContainerY(y)}>
+        <TouchableOpacity
+          style={styles.countryListContainer}
+          onPress={() => setIsDropdownOpen(!isDropdownOpen)}>
           <Image
-            source={{uri: StaticImageService.getFlagIcon()}}
+            source={{uri: StaticImageService.getFlagIcon(selectedCountry.code)}}
             style={styles.flatIcon}
           />
-          <Text></Text>
-          <MaterialIcons />
+          <Text style={styles.CountryCodeText}>
+            {selectedCountry.dial_code}
+          </Text>
+          <MaterialIcons name="keyboard-arrow-down" size={18} />
         </TouchableOpacity>
         <View style={styles.phoneInputContainer}>
-          <TextInput />
+          <TextInput
+            placeholder="Phone Number"
+            placeholderTextColor={Colors.DEFAULT_GREY}
+            selectionColor={Colors.DEFAULT_GREY}
+            keyboardType="number-pad"
+            style={styles.inputText}
+          />
         </View>
       </View>
+      {isDropdownOpen && (
+        <View style={getDropdownStyle(inputContainerY)}>
+          <FlatList
+            data={CountryCode}
+            keyExtractor={item => item.code}
+            renderItem={({item}) => (
+              <FlagItem
+                {...item}
+                onPress={country => {
+                  setSelectedCountry(country);
+                  setIsDropdownOpen(false);
+                }}
+              />
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 };
@@ -123,5 +164,29 @@ const styles = StyleSheet.create({
   flatIcon: {
     height: 20,
     width: 20,
+  },
+  CountryCodeText: {
+    fontSize: 14,
+    lineHeight: 14 * 1.4,
+    color: Colors.DEFAULT_BLACK,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+  },
+  inputText: {
+    fontSize: 18,
+    textAlignVertical: 'center',
+    padding: 0,
+    height: Display.setHeight(6),
+    color: Colors.DEFAULT_BLACK,
+  },
+  countryDropdown: {
+    backgroundColor: Colors.LIGHT_GREY,
+    position: 'absolute',
+    width: Display.setWidth(80),
+    height: Display.setHeight(50),
+    marginLeft: 20,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    borderColor: Colors.LIGHT_GREY2,
+    zIndex: 3,
   },
 });
