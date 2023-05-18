b33a037b25e94a2e1ac7794e5f591f77032d3ee6
chairil ashar <asharchairil4@gmail.com>
2023-05-18 22:24:18 +0700

    home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,46 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {Separator} from '../components';
+import {Colors, Fonts} from '../constants';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Feather from 'react-native-vector-icons/Feather';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Welcome Home</Text>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={Colors.DEFAULT_GREEN}
+        translucent
+      />
+      <Separator height={StatusBar.currentHeight} />
+      <View style={styles.backgroundCurvedContainer} />
+      <View style={styles.headerContainer}>
+        <View style={styles.locationContainer}>
+          <Ionicons
+            name="location-outline"
+            size={15}
+            color={Colors.DEFAULT_WHITE}
+          />
+          <Text style={styles.locationText}>Delivered to</Text>
+          <Text style={styles.selectedLocationText}>Home</Text>
+          <MaterialIcons
+            name="keyboard-arrow-down"
+            size={16}
+            color={Colors.DEFAULT_YELLOW}
+          />
+          <Feather
+            name="bell"
+            size={24}
+            color={Colors.DEFAULT_WHITE}
+            style={{position: 'absolute', right: 0}}
+          />
+          <View style={styles.alertBadge}>
+            <Text style={styles.alertBadgeText}>12</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,7 +50,56 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: Colors.SECONDARY_WHITE,
+  },
+  backgroundCurvedContainer: {
+    backgroundColor: Colors.DEFAULT_GREEN,
+    height: 2000,
+    position: 'absolute',
+    top: -1 * (2000 - 230),
+    width: 2000,
+    borderRadius: 2000,
+    alignSelf: 'center',
+    zIndex: -1,
+  },
+  headerContainer: {
+    justifyContent: 'space-evenly',
+  },
+  locationContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    marginHorizontal: 20,
+  },
+  locationText: {
+    color: Colors.DEFAULT_WHITE,
+    marginLeft: 5,
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+  },
+  selectedLocationText: {
+    color: Colors.DEFAULT_YELLOW,
+    marginLeft: 5,
+    fontSize: 13,
+    lineHeight: 13 * 1.4,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+  },
+  alertBadge: {
+    borderRadius: 32,
+    backgroundColor: Colors.DEFAULT_YELLOW,
     justifyContent: 'center',
     alignItems: 'center',
+    height: 16,
+    width: 16,
+    position: 'absolute',
+    right: -2,
+    top: -10,
+  },
+  alertBadgeText: {
+    color: Colors.DEFAULT_WHITE,
+    fontSize: 10,
+    lineHeight: 10 * 1.4,
+    fontFamily: Fonts.POPPINS_BOLD,
   },
 });
