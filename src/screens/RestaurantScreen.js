2e0dca90af1f643945423edb1b7e976881d61f9f
chairil ashar <asharchairil4@gmail.com>
2023-05-26 10:47:46 +0700

    slicing

--- src/screens/RestaurantScreen.js
+++ src/screens/RestaurantScreen.js
@@ -1,10 +1,86 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  ScrollView,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {RestaurantService, StaticImageService} from '../services';
+import {Display} from '../utils';
+import {ApiConstants, Colors} from '../constants';
+import {Separator} from '../components';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-const RestaurantScreen = () => {
+const RestaurantScreen = ({
+  navigation,
+  route: {
+    params: {restaurantId},
+  },
+}) => {
+  const [restaurant, setRestaurant] = useState(null);
+  useEffect(() => {
+    RestaurantService.getOneRestaurantById(restaurantId).then(response => {
+      console.log(response?.data);
+      setRestaurant(response?.data);
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <StatusBar barStyle="default" translucent backgroundColor="transparent" />
+      <>
+        <Image
+          source={{
+            uri: StaticImageService.getGalleryImage(
+              restaurant?.images?.cover,
+              ApiConstants.STATIC_IMAGE.SIZE.SQUARE,
+            ),
+          }}
+          style={styles.backgroundImage}
+        />
+        <ScrollView>
+          <Separator height={Display.setHeight(35)} />
+          <View style={styles.mainContainer}>
+            <View style={styles.titleContainer}>
+              <Text style={styles.title}>{restaurant?.name}</Text>
+              <Ionicons name="bookmark outline" />
+            </View>
+            <Text style={styles.tagText}>{restaurant?.tags?.join(' • ')}</Text>
+            <View style={styles.ratingReviewsContainer}>
+              <FontAwesome name="star" />
+              <Text style={styles.ratingText}>4.2</Text>
+              <Text style={styles.reviewsText}>(455 Reviews)</Text>
+            </View>
+            <View style={styles.deliveryDetailContainer}>
+              <View style={styles.rowAndCenter}>
+                <Image style={styles.deliveryDetailIcon} />
+                <Text style={styles.deliveryDetailText}>Free Delivery</Text>
+              </View>
+              <View style={styles.rowAndCenter}>
+                <Image style={styles.deliveryDetailIcon} />
+                <Text style={styles.deliveryDetailText}>
+                  {restaurant?.time} min
+                </Text>
+              </View>
+              <View style={styles.rowAndCenter}>
+                <Image style={styles.deliveryDetailIcon} />
+                <Text style={styles.deliveryDetailText}>
+                  {restaurant?.distance}
+                </Text>
+              </View>
+              <View style={styles.restaurantType}>
+                <Text style={styles.restaurantTypeText}>
+                  {restaurant?.type}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      </>
     </View>
   );
 };
@@ -15,6 +91,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    top: 0,
+    height: Display.setWidth(100),
+    width: Display.setWidth(100),
+  },
+  mainContainer: {
+    backgroundColor: Colors.SECONDARY_WHITE,
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
   },
 });
