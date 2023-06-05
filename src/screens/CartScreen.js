71f652f3fa4adb2ef1aaae71b4e0f8893d15a3b8
chairil ashar <asharchairil4@gmail.com>
2023-06-05 10:41:29 +0700

    button cart

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,4 +1,11 @@
-import {ScrollView, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {Colors, Fonts} from '../constants';
 import {FoodCard, Separator} from '../components';
@@ -29,13 +36,60 @@ const CartScreen = () => {
                 <FoodCard {...item?.food} key={item?.food?.id} />
               ))}
             </View>
-            <View>
-              <View>
-                <Entypo />
-                <Text></Text>
+            <View style={styles.promoCodeContainer}>
+              <View style={styles.rowAndCenter}>
+                <Entypo name="ticket" size={30} color={Colors.DEFAULT_YELLOW} />
+                <Text style={styles.promoCodeText}>Add Promo Code</Text>
               </View>
-              <Ionicons />
+              <Ionicons
+                name="chevron-forward-outline"
+                size={20}
+                color={Colors.DEFAULT_BLACK}
+              />
             </View>
+            <View style={styles.amountContainer}>
+              <View style={styles.amountSubContainer}>
+                <Text style={styles.amountLabelText}>Item Total</Text>
+                <Text style={styles.amountText}>
+                  IDR {cart?.metaData?.itemsTotal?.toFixed(2)}
+                </Text>
+              </View>
+              <View style={styles.amountSubContainer}>
+                <Text style={styles.amountLabelText}>Discount</Text>
+                <Text style={styles.amountText}>
+                  IDR {cart?.metaData?.discount?.toFixed(2)}
+                </Text>
+              </View>
+              <View style={styles.amountSubContainer}>
+                <Text style={styles.amountLabelText}>Delivery Fee</Text>
+                <Text
+                  style={{...styles.amountText, color: Colors.DEFAULT_GREEN}}>
+                  Free
+                </Text>
+              </View>
+            </View>
+            <View style={styles.totalContainer}>
+              <Text style={styles.totalText}>Total</Text>
+              <Text style={styles.totalText}>
+                IDR {cart?.metaData?.grandTotal?.toFixed(2)}
+              </Text>
+            </View>
+            <TouchableOpacity style={styles.checkoutButton}>
+              <View style={styles.rowAndCenter}>
+                <Ionicons
+                  name="cart-outline"
+                  color={Colors.DEFAULT_WHITE}
+                  size={20}
+                />
+                <Text style={styles.checkoutText}>
+                  Checkout ({cart?.cartItems?.length} Items)
+                </Text>
+              </View>
+              <Text style={styles.checkoutText}>
+                IDR {cart?.metaData?.grandTotal?.toFixed(2)}
+              </Text>
+            </TouchableOpacity>
+            <Separator height={Display.setHeight(9)} />
           </ScrollView>
         </>
       ) : (
@@ -67,5 +121,74 @@ const styles = StyleSheet.create({
   },
   foodList: {
     marginHorizontal: Display.setWidth(4),
+  },
+  promoCodeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: Display.setWidth(4),
+    paddingVertical: 15,
+    marginTop: 10,
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+    justifyContent: 'space-between',
+  },
+  promoCodeText: {
+    fontSize: 15,
+    fontFamily: Fonts.POPPINS_MEDIUM,
+    lineHeight: 15 * 1.4,
+    color: Colors.DEFAULT_BLACK,
+    marginLeft: 10,
+  },
+  rowAndCenter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  amountContainer: {
+    marginHorizontal: Display.setWidth(4),
+    paddingVertical: 20,
+    borderBottomWidth: 0.5,
+  },
+  amountSubContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 3,
+  },
+  amountLabelText: {
+    fontSize: 15,
+    fontFamily: Fonts.POPPINS_SEMI_BOLD,
+    lineHeight: 15 * 1.4,
+    color: Colors.DEFAULT_GREEN,
+  },
+  amountText: {
+    fontSize: 15,
+    fontFamily: Fonts.POPPINS_SEMI_BOLD,
+    lineHeight: 15 * 1.4,
+    color: Colors.DEFAULT_BLACK,
+  },
+  totalContainer: {
+    marginHorizontal: Display.setWidth(4),
+    paddingVertical: 15,
+    borderBottomWidth: 0.5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  totalText: {
+    fontSize: 20,
+    fontFamily: Fonts.POPPINS_SEMI_BOLD,
+    lineHeight: 20 * 1.4,
+    color: Colors.DEFAULT_BLACK,
+  },
+  checkoutButton: {
+    flexDirection: 'row',
+    width: Display.setWidth(80),
+    backgroundColor: Colors.DEFAULT_GREEN,
+    alignSelf: 'center',
+    paddingHorizontal: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 10,
+    height: Display.setHeight(7),
+    marginTop: 10,
   },
 });
