eec5cd1115d02cb6a2d25a7a8feca2ea78e5ba42
chairil ashar <asharchairil4@gmail.com>
2023-06-10 22:38:49 +0700

    bookmark screen

--- src/navigators/BottomTabs.js
+++ src/navigators/BottomTabs.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {CartScreen, HomeScreen} from '../screens';
+import {BookmarkScreen, CartScreen, HomeScreen} from '../screens';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Display} from '../utils';
 import {Colors} from '../constants';
@@ -29,6 +29,15 @@ export default () => (
       options={{
         tabBarIcon: ({color}) => (
           <Ionicons name="home-outline" size={23} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Bookmark"
+      component={BookmarkScreen}
+      options={{
+        tabBarIcon: ({color}) => (
+          <Ionicons name="bookmark-outline" size={23} color={color} />
         ),
       }}
     />
